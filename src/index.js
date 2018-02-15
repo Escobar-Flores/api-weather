bc024ec52f58f16b84b63f0ffa7d37037a607dad
Rocciescobar <roccisen.88@gmail.com>
2018-02-15 01:08:18 -0500

    Añade funcionalidad de geolocalización y modifica index.html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('Listo');
+// Obtener ubicación actual de usuario
+
+function initMap() {
+  function search() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(functionSuccess, functionError);
+    }
+  };
+  
+  document.getElementById('btn').addEventListener('click', search);
+  
+  let myLatit, myLongit;
+  let functionSuccess = (position) => {
+    myLatit = position.coords.latitude;
+    myLongit = position.coords.longitude;
+
+    localStorage.setItem('Latitud', myLatit);
+    localStorage.setItem('Longitud', myLongit);
+
+    window.location.href = 'views/daily/index.html';
+  };
+  
+  let functionError = (error) => {
+    alert('Tenemos un problema con encontrar tu ubicación');
+  };
+};
