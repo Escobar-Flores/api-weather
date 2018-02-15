ec1d922fde035f0f8b95d2fd11f825151043f595
Manuela <fmanuela499@gmail.com>
2018-02-15 10:10:23 -0500

    agregando funcionalidad clima diario

--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,44 @@
 'use strict';
 
+// funcion que obtiene el clima :
+
+function getWeather(data) {
+  var route = data.daily.data[0];
+  // utilizando la data
+  var icon = route.icon;
+  var template = '<img src="../../assets/images/' + icon + '.png" alt="" class="img-size">';
+  var summary = '"' + route.summary + '"';
+  var speedWind = route.windSpeed + ' m/s';
+  var humidity = route.humidity * 100 + ' %';
+  var uvIndex = route.uvIndex;
+  var maxTemp = ((route.apparentTemperatureMax - 32) / 1.8).toFixed(0) + '°';
+  var pressure = route.pressure + ' Pa';
+  // llamando los elementos del dom y agregando los valores de la data:
+  $('.max-temp-js').text(maxTemp);
+  $('.summary-js').text(summary);
+  $('.speed-wind-js').text(speedWind);
+  $('.humidity-js').text(humidity);
+  $('.uv-index-js').text(uvIndex);
+  $('.pressure-js').text(pressure);
+  $('.container-image-js').append(template);
+};
+
 // Obtener ubicación actual de usuario
 
 function initMap() {
   function search() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(functionSuccess, functionError);
-    }
+    };
   };
 
   document.getElementById('btn').addEventListener('click', search);
 
   var myLatit = void 0,
-      myLongit = void 0;
+    myLongit = void 0;
   var functionSuccess = function functionSuccess(position) {
     myLatit = position.coords.latitude;
     myLongit = position.coords.longitude;
-
-    console.log(myLatit);
-    console.log(myLongit);
 
     localStorage.setItem('Latitud', myLatit);
     localStorage.setItem('Longitud', myLongit);
@@ -30,3 +50,12 @@ function initMap() {
     alert('Tenemos un problema con encontrar tu ubicación');
   };
 };
+
+$(document).ready(function() {
+  var proxy = 'https://cors-anywhere.herokuapp.com/';
+  var apiLinkDS = 'https://api.darksky.net/forecast/bcfb332dbff54b0b470a2c8cbad6e360/' + localStorage.getItem('Latitud') + ',' + localStorage.getItem('Longitud') + '?lang=es';
+  $.ajax({
+    url: proxy + apiLinkDS,
+    success: getWeather
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
+
+// funcion que obtiene el clima :
+
+function getWeather(data) {
+  let route = data.daily.data[0];
+  // utilizando la data
+  let icon = route.icon;
+  let template = `<img src="../../assets/images/${icon}.png" alt="" class="img-size">`; 
+  let summary = '"' + route.summary + '"';
+  let speedWind = route.windSpeed + ' m/s';
+  let humidity = (route.humidity * 100) + ' %';
+  let uvIndex = route.uvIndex;
+  let maxTemp = ((route.apparentTemperatureMax - 32) / 1.8).toFixed(0) + '°';
+  let pressure = route.pressure + ' Pa';
+  // llamando los elementos del dom y agregando los valores de la data:
+  $('.max-temp-js').text(maxTemp);
+  $('.summary-js').text(summary);
+  $('.speed-wind-js').text(speedWind);
+  $('.humidity-js').text(humidity);
+  $('.uv-index-js').text(uvIndex);
+  $('.pressure-js').text(pressure);
+  $('.container-image-js').append(template);
+};
+
 // Obtener ubicación actual de usuario
 
 function initMap() {
   function search() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(functionSuccess, functionError);
-    }
+    };
   };
   
   document.getElementById('btn').addEventListener('click', search);
@@ -24,3 +48,12 @@ function initMap() {
     alert('Tenemos un problema con encontrar tu ubicación');
   };
 };
+
+$(document).ready(function() {
+  var proxy = 'https://cors-anywhere.herokuapp.com/';
+  var apiLinkDS = `https://api.darksky.net/forecast/bcfb332dbff54b0b470a2c8cbad6e360/${localStorage.getItem('Latitud')},${localStorage.getItem('Longitud')}?lang=es`;
+  $.ajax({
+    url: proxy + apiLinkDS,
+    success: getWeather
+  });
+});
